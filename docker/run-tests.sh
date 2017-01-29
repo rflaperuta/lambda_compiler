88c290441b27a3ba397518a4fd49ea60e0f1fd82
Rodrigo Falcetta Laperuta <rflaperuta@gmail.com>
2017-01-29 12:12:13 -0200

    Fix order of things

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 	echo 'COMPOSE BUILD'
 	sudo docker-compose build compiler
+
 	echo 'COMPOSE RUN'
+	sudo docker-compose run compiler	
 	sudo docker-compose run -e DEBUG='*' compiler node index.js
+
 	echo 'TEAR DOWN'
 	sudo docker-compose down 
   sudo docker-compose stop
