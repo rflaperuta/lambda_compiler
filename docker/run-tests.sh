4181470592df0fe8b8b2b8236d4c50f7b9024451
Rodrigo Falcetta Laperuta <rflaperuta@gmail.com>
2017-01-29 11:44:47 -0200

    Fix docker compose 2

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 	echo 'COMPOSE BUILD'
-	sudo docker-compose build --force-rm --no-cache --pull compiler
+	sudo docker-compose build compiler
 	echo 'COMPOSE RUN'
 	sudo docker-compose run -e DEBUG='*' compiler node index.js
 	echo 'TEAR DOWN'
-	sudo docker-compose down compiler
+	sudo docker-compose down 
   sudo docker-compose stop
