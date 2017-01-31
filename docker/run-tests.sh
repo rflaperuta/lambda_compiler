5b8084c6c8fcdc0ed77bcac4ea0883bc8bcdd9aa
Rodrigo Falcetta Laperuta <rflaperuta@gmail.com>
2017-01-30 22:49:56 -0200

    test additions

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -6,9 +6,12 @@
 	sudo docker-compose run compiler	
 	sudo docker-compose run -e DEBUG='*' compiler node index.js
 	sudo docker-compose run -e DEBUG='*' compiler npm run test
+	test_result=$?
 
 	if [[ -e test-results.xml ]]; then cp test-results.xml ${CIRCLE_TEST_REPORTS}/test-results.xml; fi
 
 	echo 'TEAR DOWN'
 	sudo docker-compose down 
   sudo docker-compose stop
+
+	exit ${test_result}
